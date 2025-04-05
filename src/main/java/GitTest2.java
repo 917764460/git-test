b957e0a35c1b6238ad6d355a644f4b6b03891a85
AU <AU@gmail.com>
2025-04-05 10:53:42 +0800

    fifth commit

---
+++ src/main/java/GitTest2.java
@@ -0,0 +1,13 @@
+/**
+ * 无限进步!!!
+ * Description:
+ *
+ * @Author AU
+ * @Create: 2025-04-05 10:53
+ * @Version: 1.0
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("蔡徐坤");
+    }
+}
