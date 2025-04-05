290aa7c1b45e12d540f72d915c9cbb71c6fa0f21
AU <AU@gmail.com>
2025-04-05 10:36:11 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,13 @@
+/**
+ * 无限进步!!!
+ * Description:
+ *
+ * @Author AU
+ * @Create: 2025-04-05 10:35
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("丢手绢～～～");
+    }
+}
